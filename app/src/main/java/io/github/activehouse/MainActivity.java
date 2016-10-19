9b58945f93f3df6892e33e9d24ccb2629bc86fab
irwintr <trentonv17@gmail.com>
2016-10-18 21:18:21 -0400

    Added save login info

--- app/src/main/java/io/github/activehouse/MainActivity.java
+++ app/src/main/java/io/github/activehouse/MainActivity.java
@@ -1,7 +1,11 @@
 package io.github.activehouse;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
 
 
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+
+        //String highScore = sharedPref.getString("username", 0);
+
+    }
+
+    public void loginButtonClick(View view) {
+        SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("username", "Trent");
+        editor.putString("password", "Password");
+        editor.commit();
     }
 }
