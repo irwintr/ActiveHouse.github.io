58f12ff468235f27169bb5a8f7827f1fe36fcdc7
irwintr <trentonv17@gmail.com>
2016-10-19 20:35:02 -0400

    Added room and roomview pages and some UI changes

--- app/src/main/java/io/github/activehouse/HomeActivity.java
+++ app/src/main/java/io/github/activehouse/HomeActivity.java
@@ -67,7 +67,7 @@ public class HomeActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
             return true;
         }
 
